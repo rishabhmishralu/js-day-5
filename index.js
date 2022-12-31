5581bbe66b6b351c0f3d274755de4b36143d26aa
= <=>
2023-01-01 00:53:47 +0530

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+// async function sum(a,b){
+//     c=a+b;
+//    await console.log(c)
+//    function multi(){
+//     console.log(a*b);
+//    }
+//    multi()
+// }
+// sum(2,4)
+
+// console.log("rtyui");
+
+
+// map &filter 
+// const arr=[1,2,3,4,4,5,3]
+// c=arr.sort((a,b)=>b-a)
+// 
+// sum()
+// function sum(){
+//     console.log("dfghjkl;");
+// }
+// console.log(x);
+// var x=10
+ 
+//  async function sum(a,b){
+//   await console.log(a+b);
+//     console.log(a*b)
+    
+//  }
+//  sum(2,4)
+//  console.log("multi");
